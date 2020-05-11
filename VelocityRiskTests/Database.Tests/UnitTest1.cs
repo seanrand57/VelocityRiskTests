8c31284c7aaf4d2d41af474df18e7a1eab8b54d7
Pavel Zayats <pzayats@scnsoft.com>
2020-05-11 16:05:42 +0300

    initial commit

---
+++ VelocityRiskTests/Database.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Database.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
