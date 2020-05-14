1fc69a0b0ff8e6fafd926f5735c221d9b8e1ca73
Pavel Zayats <pzayats@scnsoft.com>
2020-05-14 15:43:57 +0300

    added page objects for Customers > Make a Payment page

---
+++ VelocityRiskTests/Ui.Tests/PageObjectModels/MakePaymentPage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Ui.Tests.PageObjectModels
+{
+    public class MakePaymentPage : BasePage
+    {
+        [FindsBy(How = How.XPath, Using = "//input[@value='Pay with Check']")]
+        private IWebElement _payWithCheck;
+
+        [FindsBy(How = How.XPath, Using = "//input[@value='Pay with Credit Card']")]
+        private IWebElement _payWithCreditCard;
+
+        public MakePaymentPage(IWebDriver driver) : base(driver)
+        {
+            PageUrl = "https://portal.icheckgateway.com/Velocity/";
+        }
+    }
+}
